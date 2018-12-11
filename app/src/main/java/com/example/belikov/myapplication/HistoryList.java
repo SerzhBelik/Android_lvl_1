1a9a1688328a8c9c92ec643dd9571943aea6995e
SerzhBelik <sergeybelik86@gmail.com>
2018-12-11 23:55:59 +0400

    ДЗ № 7 пп 2 и 3 закончены

--- app/src/main/java/com/example/belikov/myapplication/HistoryList.java
+++ app/src/main/java/com/example/belikov/myapplication/HistoryList.java
@@ -1,8 +1,10 @@
 package com.example.belikov.myapplication;
 
 import android.os.Bundle;
+import android.os.Parcel;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -17,9 +19,14 @@ import java.util.Random;
 
 public class HistoryList extends AppCompatActivity {
 
-    private String[] data = {"+10","+11","+9","+12","+10","+11","+10","+12","+8","+9"};
-    private List<String> dataList = new ArrayList<>(Arrays.asList(data));
+    private String[] dataTemp = {"+10","+11","+9","+12","+10","+11","+10","+12","+8","+9"};
+    private String[] dataWind = {"5 м/с","3 м/с","4 м/с","1 м/с","2 м/с","0 м/с","3 м/с","7 м/с","5м/с","3м/с"};
+    private String[] dataHumid = {"60%","62%","70%","58%","78%","85%","60%","80%","55%","67%"};
+    private String[] dataPres = {"755 мм рт. ст.","756 мм рт. ст.","758 мм рт. ст.","760 мм рт. ст.",
+            "762 мм рт. ст.","755 мм рт. ст.","753 мм рт. ст.","750 мм рт. ст.","755 мм рт. ст.","756 мм рт. ст."};
+    private List<String> dataList;
     HistoryAdapter adapter;
+    int currentPosition;
 
 
     @Override
@@ -28,6 +35,7 @@ public class HistoryList extends AppCompatActivity {
         setContentView(R.layout.actitvity_history_list);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         // Эта установка служит для повышения производительности системы
         recyclerView.setHasFixedSize(true);
@@ -38,15 +46,55 @@ public class HistoryList extends AppCompatActivity {
 
         // Установим адаптер
 
+        setDataList();
         adapter = new HistoryAdapter(dataList);
         recyclerView.setAdapter(adapter);
     }
 
     public void onClickMoreDays(View view){
-        Random rnd = new Random(System.currentTimeMillis());
-// Получаем случайное число в диапазоне от min до max (включительно)
-        int number = 5 + rnd.nextInt(10);
-        dataList.add("+" + number);
+
+
+        String s = newElement(currentPosition);
+        dataList.add(0, s);
         adapter.notifyDataSetChanged();
+    }
+
+    private void setDataList(){
+        MyParcel parcel = getIntent().getParcelableExtra(MainActivity.TEXT);
+        currentPosition = parcel.currentPosition;
+        switch (currentPosition){
+            case 0:
+                dataList = new ArrayList<>(Arrays.asList(dataTemp));
+                break;
+            case 1:
+                dataList = new ArrayList<>(Arrays.asList(dataWind));
+                break;
+            case 2:
+                dataList = new ArrayList<>(Arrays.asList(dataHumid));
+                break;
+            case 3:
+                dataList = new ArrayList<>(Arrays.asList(dataPres));
+                break;
+        }
+    }
+
+    private String newElement(int currentPosition){
+        String s = null;
+        Random rnd = new Random(System.currentTimeMillis());
+        switch (currentPosition){
+            case 0:
+                s = "+" + 5 + (rnd.nextInt(10));
+                break;
+            case 1:
+                s = ((rnd.nextInt(10))) + " м/с";
+                break;
+            case 2:
+                s = (20 + (rnd.nextInt(80))) + "%";
+                break;
+            case 3:
+                s = (740 + (rnd.nextInt(40))) + " мм рт. ст.";
+                break;
+        }
+        return s;
     }
 }
--- app/src/main/java/com/example/belikov/myapplication/WeatherParam.java
+++ app/src/main/java/com/example/belikov/myapplication/WeatherParam.java
@@ -7,20 +7,22 @@ import android.view.View;
 import android.widget.Toast;
 
 public class WeatherParam extends AppCompatActivity {
+//  private int position;
+  MyParcel parcel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.param_fragment);
 
+    parcel = getIntent().getParcelableExtra(MainActivity.TEXT);
+//  position = parcel.currentPosition;
 
-//    MyParcel parcel = getIntent().getParcelableExtra(MainActivity.TEXT);
-
-//    Toast.makeText(getApplicationContext(), "onCreate(), " + parcel.weatherArr[1], Toast.LENGTH_SHORT).show();
   }
 
   public void onClickHistory(View view){
     Intent intent = new Intent(this, HistoryList.class);
+    intent.putExtra(MainActivity.TEXT, parcel);
     startActivity(intent);
 
   }
