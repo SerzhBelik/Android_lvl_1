c59fa12d77ea50b24e36c5998fe4c7d9619f935b
Belikov <Sergeybelik86@gmail.com>
2018-12-17 18:10:27 +0300

    ДЗ №8 поведение добавлено но не работает

--- app/src/main/java/com/example/belikov/myapplication/MainActivity.java
+++ app/src/main/java/com/example/belikov/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.belikov.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,55 +8,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final static String TEXT_CONTENT = "В 2014 году Google представила новый подход к дизайну приложений. " +
-            "Это была попытка сделать единообразный интерфейс для всех приложений Google и Android. " +
-            "Интерфейс, который выглядел бы одинаково, независимо от того, где работает приложение: " +
-            "на телефоне, планшете или компьютере. Материальный дизайн основан на образе бумаги на экране. " +
-            "Ее называют квантовой или цифровой. Бумага тонкая, плоская, но расположена в трехмерном пространстве, " +
-            "имеет тени и двигается. В таком образе предстают объекты интерфейса, " +
-            "а анимация показывает пользователю, что происходит. Чрезмерная анимация не нужна, " +
-            "Потому что никому не интересно ждать, пока окно с сообщением налетается по экрану.\n";
-//    В 2014 году Google представила новый подход к дизайну приложений. Это была попытка сделать единообразный интерфейс для всех приложений Google и Android. Интерфейс, который выглядел бы одинаково, независимо от того, где работает приложение: на телефоне, планшете или компьютере.
-//    Материальныйериальный дизайн основан на образе бумаги на экране. Ее называют квантовой или цифровой. Бумага тонкая, плоская, но расположена в трехмерном пространстве, имеет тени и двигается. В таком образе предстают объекты интерфейса, а анимация показывает пользователю, что происходит. Чрезмерная анимация не нужна, потому что никому никому не интересно ждать, пока окно с сообщением налетается по экрану.
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-//        TextView content = findViewById(R.id.content);
-//        content.setText(TEXT_CONTENT);
+        // Установка Action Bar на основе Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // обработка кликов по FAB кнопке
         FloatingActionButton fab = findViewById(R.id.fab);
-        // Обработка нажатия на плавающую кнопку
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Здесь вылетит Snackbar
-                Snackbar.make(view, "Вы нажали на плавающую кнопку", Snackbar.LENGTH_LONG).show();
+                // Вывести Snackbar
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Размещение меню в action bar
-        // если он присутствует.
+        // разместить меню
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пунктов меню
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+            return true;
 
         return super.onOptionsItemSelected(item);
     }
